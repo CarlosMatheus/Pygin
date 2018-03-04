6afed5e1cbf96cc9d6c4be53778f49207a8bc18d
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-03-04 20:34:13 -0300

    Create NormalBehavior class

---
+++ game_engine/normal_behavior.py
@@ -0,0 +1,11 @@
+class NormalBehavior:
+
+    def start(self):
+        """
+        Will be called just once when the NormalBehavior is instantiate on scene
+        """
+
+    def update(self):
+        """
+        Will be call every frame
+        """
