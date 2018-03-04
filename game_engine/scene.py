22766228438b9e3ba88ee509783093a45d45fd59
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-03-04 19:22:37 -0300

    Create class scene

---
+++ game_engine/scene.py
@@ -0,0 +1,86 @@
+import pygame
+from .game_object import GameObject
+from .time import Time
+from .input import Input
+from .draw import Draw
+
+
+class Scene:
+
+    def __init__(self, _normal_behaviors):
+        """
+        Set object's variables to start a new scene
+        :param _normal_behaviors: list of all _normal_behaviors of the scene
+        """
+        self.normal_behaviors = _normal_behaviors
+        self.frame_events = []
+        self.end_scene = False
+
+    def start(self):
+        """
+        Run methods to set the scene up
+        """
+        self.run_events()
+        Draw.update_background()
+        self.run_all_starts()
+        pygame.display.flip()
+        Time.end_of_start()
+
+    def run_all_starts(self):
+        """
+        Run the start method of each normal_behavior
+        """
+        for normal_behavior in self.normal_behaviors:
+            normal_behavior.start()
+
+    def run_all_updates(self):
+        """
+        Runs the update of each normal_behavior of the scene
+        """
+        for normal_behavior in self.normal_behaviors:
+            normal_behavior.update()
+
+    def draw_all_game_objects(self):
+        """
+        Run draw method of each game_object of the scene
+        """
+        for normal_behavior in self.normal_behaviors:
+            if isinstance(normal_behavior, GameObject):
+                normal_behavior.draw_game_object()
+
+    def scene_loop(self):
+        """
+        Defines the main loop of the scene
+        The scene occurs while in the loop
+        """
+        while not self.end_scene:
+            self.run_events()
+            Draw.update_background()
+            self.run_all_updates()
+            self.draw_all_game_objects()
+            pygame.display.flip()
+            Time.end_of_loop()
+
+    def add_game_object(self, game_object):
+        """
+        Add a new game object to the scene's normal_behaviors list
+        :param game_object: new game_object to add to scene
+        """
+        self.normal_behaviors.append(game_object)
+        game_object.start()
+
+    def run_events(self):
+        """
+        get the events in pygame queue
+        and run the methods related to them
+        """
+        self.frame_events = pygame.event.get()
+        Input.update_input(self.frame_events)
+
+    def debug_event(self):
+        """
+        DEBUG print all the events of each frame
+        :return:
+        """
+        for event in self.frame_events:
+            print(event)
