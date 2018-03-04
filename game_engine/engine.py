5e58fdd0ea636183c9fb24d29c34689392f832b6
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-03-04 19:05:16 -0300

    Ajust engine

--- game_engine/engine.py
+++ game_engine/engine.py
@@ -1,20 +1,40 @@
 import pygame
+from .draw import Draw
+
 
 class Engine:
 
+    screen_width = 360
+    screen_height = 640
     game_display = 0
-    screen_width = 300
-    screen_height = 600
+    scenes_list = []
 
-    @staticmethod
-    def start_game(game_name):
+    @classmethod
+    def start_game(cls, game_name, scenes):
+        """
+        Start the game with pygame
+        :param game_name: name that will be displayed on the window
+        :param scenes: list of scenes of the game
+        """
         pygame.init()
-        Engine.game_display = pygame.display.set_mode( (Engine.screen_width, Engine.screen_height) )
+        cls.game_display = pygame.display.set_mode((cls.screen_width, cls.screen_height))
         pygame.display.set_caption(game_name)
+        cls.scenes_list = scenes
+        Draw.set_game_display(Engine.game_display)
+        cls.start_first_scene()
 
+    @classmethod
+    def start_first_scene(cls):
+        """
+        Start the first scene
+        """
+        Engine.scenes_list[0].start()
+        Engine.scenes_list[0].scene_loop()
 
-    @staticmethod
-    def end_game():
-        asdf = 0
-
-
+    @classmethod
+    def end_game(cls):
+        """
+        Quits the game
+        """
+        pygame.quit()
+        quit()
