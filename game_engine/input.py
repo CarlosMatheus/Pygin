16dbf6e6d4eab2462f3f85b640b3ce0a9b3e4ede
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-03-04 19:48:14 -0300

    Created input class

---
+++ game_engine/input.py
@@ -0,0 +1,55 @@
+import pygame
+from .engine import Engine
+
+
+class Input:
+
+    is_pressing_left = False
+    is_pressing_right = False
+
+    @classmethod
+    def update_input(cls, events):
+        """
+        find on the events list all events to update the input
+        :param events: events list from pygame queue
+        """
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                cls.__key_down(event)
+            elif event.type == pygame.KEYUP:
+                cls.__key_up(event)
+            elif event.type == pygame.QUIT:
+                cls.__quit_game()
+
+    @classmethod
+    def __key_down(cls, event):
+        """
+        player started to press a key
+        set it to true
+        :param event: keydown event
+        """
+        if event.key == pygame.K_LEFT:
+            cls.is_pressing_left = True
+        if event.key == pygame.K_RIGHT:
+            cls.is_pressing_right = True
+
+    @classmethod
+    def __key_up(cls, event):
+        """
+        player stopped to press a key
+        set it to false
+        :param event: keyup event
+        """
+        if event.key == pygame.K_LEFT:
+            cls.is_pressing_left = False
+        if event.key == pygame.K_RIGHT:
+            cls.is_pressing_right = False
+
+    @classmethod
+    def __quit_game(cls):
+        """
+        if pressed quit key
+        call the engine's method to quit
+        """
+        Engine.end_game()
+
