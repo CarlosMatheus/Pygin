26ac65715a66a677a978e6b5d5b88f13f2ce141e
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-03-04 20:20:07 -0300

    Create GameObject class

---
+++ game_engine/game_object.py
@@ -0,0 +1,27 @@
+from .normal_behavior import NormalBehavior
+from .draw import Draw
+from .components.transform import Transform
+from .components.material import Material
+
+
+class GameObject(NormalBehavior):
+
+    def __init__(self, position_x, position_y, rotation, scale_x, scale_y, color):
+        """
+        set basics game_objects parameters
+        :param position_x: game_object's x initial position
+        :param position_y: game_object's y initial position
+        :param rotation: game_object's initial rotation in degrees
+        :param scale_x: game_object's x initial scale
+        :param scale_y: game_object's y initial scale
+        :param color: game_object's initial material color
+        """
+        self.transform = Transform(position_x, position_y, rotation, scale_x, scale_y)
+        self.material = Material(color)
+
+    def draw_game_object(self):
+        """
+        Draw the game_object on screen
+        """
+        Draw.rect(self.transform.translate[0], self.transform.translate[1], self.transform.scale[0],
+                  self.transform.scale[1], self.material.color)
