33bcd4deacea0115248bbd1e411a0509ae2e0b15
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-03-04 19:09:51 -0300

    Add scenes controler to balance file

--- balance.py
+++ balance.py
@@ -1,5 +1,6 @@
 from game_engine.engine import Engine
+from elements.scenes.scenes import ScenesController
 
 game_name = "Balance"
 
-Engine.start_game(game_name)
+Engine.start_game(game_name, ScenesController.get_scenes())
