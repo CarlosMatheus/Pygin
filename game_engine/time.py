21ccca29271be13c5307c7c40fa34f6d5c9ee688
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-03-04 22:15:57 -0300

    Refactor methods

--- game_engine/time.py
+++ game_engine/time.py
@@ -2,26 +2,31 @@ import pygame
 
 
 class Time:
-    get_ticks_last_frame = pygame.time.get_ticks()
+    last_frame_tick = pygame.time.get_ticks()
+    clock = pygame.time.Clock()
 
     @classmethod
     def end_of_loop(cls):
         """
         Set the time at the moment to get_ticks_last_frame
         """
-        cls.get_ticks_last_frame = pygame.time.get_ticks()
+        cls.last_frame_tick = pygame.time.get_ticks()
+        cls.clock.tick(200)
 
     @classmethod
     def end_of_start(cls):
         """
         Set the time at the moment to get_ticks_last_frame
         """
-        cls.get_ticks_last_frame = pygame.time.get_ticks()
+        cls.last_frame_tick = pygame.time.get_ticks()
+        cls.clock.tick(200)
 
     @classmethod
     def delta_time(cls):
         """
         :return: the duration of that frame in seconds
         """
-        return (pygame.time.get_ticks() - cls.get_ticks_last_frame) / 1000.0
-
+        if cls.clock.get_fps() != 0:
+            return 1/cls.clock.get_fps()
+        else:
+            return 0
