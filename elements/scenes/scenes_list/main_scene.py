93079389906088209c5c9f841a7e2c32f9f82413
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-03-04 22:03:51 -0300

    Create MainScene scene

---
+++ elements/scenes/scenes_list/main_scene.py
@@ -0,0 +1,16 @@
+from game_engine.scene import Scene
+from game_engine.color import Color
+from elements.normal_behaviors.game_objects.square_test import SquareTest
+from elements.normal_behaviors.game_objects.square_test_falling import SquareTestFalling
+
+
+class MainScene(Scene):
+
+    def __init__(self):
+        self.game_object_list = [SquareTest(30, 40, 0, 50, 40, Color.white),
+                                 SquareTestFalling(123, 234, 0, 12, 40, Color.red),
+                                 SquareTestFalling(342, 123, 0, 50, 32, Color.red),
+                                 SquareTestFalling(12, -123, 0, 50, 32, Color.red),
+                                 SquareTestFalling(342, -232, 0, 50, 32, Color.red)
+                                 ]
+        super(MainScene, self).__init__(self.game_object_list)
