0891046b80fd1b582bfdb0f82d8bddb854a7402c
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-03-06 09:32:44 -0300

    Resolve conflict

--- elements/scenes/scenes_list/main_scene.py
+++ elements/scenes/scenes_list/main_scene.py
@@ -1,5 +1,7 @@
 from game_engine.scene import Scene
-from game_engine.color import Color
+from elements.normal_behaviors.player_controller import PlayerController
+from elements.normal_behaviors.game_objects.rectangle import Rectangle
+from elements.normal_behaviors.game_objects.circle import Circle
 from elements.normal_behaviors.game_objects.square_test import SquareTest
 from elements.normal_behaviors.game_objects.square_test_falling import SquareTestFalling
 
@@ -10,10 +12,6 @@ class MainScene(Scene):
         """
         Create the list of normal_behaviors and call the superclass constructor passing the list
         """
-        self.normal_behaviors_list = [SquareTest(30, 40, 0, 50, 40, Color.white),
-                                      SquareTestFalling(123, 234, 0, 12, 40, Color.red),
-                                      SquareTestFalling(342, 123, 0, 50, 32, Color.red),
-                                      SquareTestFalling(12, -123, 0, 50, 32, Color.red),
-                                      SquareTestFalling(342, -232, 0, 50, 32, Color.red)
-                                      ]
+        self.normal_behaviors_list = [PlayerController()]
+
         super(MainScene, self).__init__(self.normal_behaviors_list)
--- game_engine/game_object.py
+++ game_engine/game_object.py
@@ -1,7 +1,6 @@
 from .normal_behavior import NormalBehavior
 from .draw import Draw
 from .components.transform import Transform
-from .components.material import Material
 from .engine import Engine
 
 
@@ -9,7 +8,7 @@ class GameObject(NormalBehavior):
 
     current_running_scene = 0
 
-    def __init__(self, position_x, position_y, rotation, scale_x, scale_y, color):
+    def __init__(self, position_x, position_y, rotation, scale_x, scale_y):
         """
         set basics game_objects parameters
         :param position_x: game_object's x initial position
@@ -18,16 +17,22 @@ class GameObject(NormalBehavior):
         :param scale_x: game_object's x initial scale
         :param scale_y: game_object's y initial scale
         :param color: game_object's initial material color
+        :param type: string representing the object type (Circle or Rectangle)
         """
         self.transform = Transform(position_x, position_y, rotation, scale_x, scale_y)
-        self.material = Material(color)
 
     def draw_game_object(self):
         """
         Draw the game_object on screen
         """
-        Draw.rect(self.transform.translate[0], self.transform.translate[1], self.transform.scale[0],
-                  self.transform.scale[1], self.material.color)
+        if hasattr(self, 'rectangle_mash'):
+            Draw.rect(self.transform.translate[0], self.transform.translate[1],
+                      self.transform.scale[0] * self.rectangle_mash.width,
+                      self.transform.scale[1] * self.rectangle_mash.height, self.rectangle_mash.color)
+
+        elif hasattr(self, 'circle_mash'):
+            Draw.circle(self.circle_mash.position_x, self.circle_mash.position_y,
+                        self.circle_mash.radius, self.circle_mash.color)
 
     @classmethod
     def instantiate(cls, game_object):
