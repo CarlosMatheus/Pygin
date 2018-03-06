0dace888b548dd712f5ab54e9754e5167e39d413
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-03-05 22:02:29 -0300

    Create random color method

--- game_engine/color.py
+++ game_engine/color.py
@@ -1,4 +1,10 @@
+import random
+
 class Color:
     white = (255, 255, 255)
     black = (0, 0, 0)
     red = (255, 0, 0)
+
+    @classmethod
+    def random_color(cls):
+        return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
