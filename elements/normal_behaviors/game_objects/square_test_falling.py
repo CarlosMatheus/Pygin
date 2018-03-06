c7f071f75ef484872fdac71c7a63c1f8b09a37a5
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-03-06 08:49:15 -0300

    Destroy rectangle if it exit the screen

--- elements/normal_behaviors/game_objects/square_test_falling.py
+++ elements/normal_behaviors/game_objects/square_test_falling.py
@@ -1,5 +1,6 @@
 from game_engine.time import Time
 from game_engine.game_object import GameObject
+from game_engine.engine import Engine
 
 
 class SquareTestFalling(GameObject):
@@ -9,14 +10,20 @@ class SquareTestFalling(GameObject):
         NomalBehaivor start method
         will be called when the object is instantiate on scene
         """
-        self.fall_velocity = 25
+        self.fall_velocity = 250
 
     def update(self):
         """
         NomalBehaivor update method
         will be call every frame
         """
+        if self.is_out_of_screen():
+            GameObject.destroy(self)
         self.fall()
+
+    def is_out_of_screen(self):
+
+        return self.transform.translate[1] > Engine.screen_height
 
     def fall(self):
         """
