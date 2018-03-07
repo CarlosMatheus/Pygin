409e4aeff74c81a342951deb6e56e021b961ea17
Eric Moreira <ericpqmor@gmail.com>
2018-03-07 01:04:51 -0300

    Scene is now common gameplay

--- elements/scenes/scenes_list/main_scene.py
+++ elements/scenes/scenes_list/main_scene.py
@@ -1,9 +1,9 @@
 from elements.normal_behaviors.test_rect_generator import TestRectGenerator
+from game_engine.scene import Scene
+from elements.normal_behaviors.player_controller import PlayerController
 from elements.normal_behaviors.obstacles_controller import ObstaclesController
 from elements.normal_behaviors.game_objects.rectangle import Rectangle
 from elements.normal_behaviors.game_objects.circle import Circle
-from elements.normal_behaviors.game_objects.square_test import SquareTest
-from elements.normal_behaviors.game_objects.square_test_falling import SquareTestFalling
 
 
 class MainScene(Scene):
@@ -12,6 +12,5 @@ class MainScene(Scene):
         """
         Create the list of normal_behaviors and call the superclass constructor passing the list
         """
-        self.normal_behaviors_list = [PlayerController(), TestRectGenerator()]
-        # self.normal_behaviors_list = [PlayerController(), ObstaclesController()]
+        self.normal_behaviors_list = [PlayerController(), ObstaclesController()]
         super(MainScene, self).__init__(self.normal_behaviors_list)
