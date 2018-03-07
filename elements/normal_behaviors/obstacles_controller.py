08a8ea283192454ec7f8ea3b658c29be32e3d18c
Eric Moreira <ericpqmor@gmail.com>
2018-03-07 01:05:07 -0300

    Refactoring obstacles controller

--- elements/normal_behaviors/obstacles_controller.py
+++ elements/normal_behaviors/obstacles_controller.py
@@ -1,9 +1,10 @@
-from game_engine.input import Input
+from pygame.math import Vector2
 from game_engine.time import Time
 from random import randint as rand
 from game_engine.engine import Engine
 from game_engine.normal_behavior import NormalBehavior
 from elements.normal_behaviors.game_objects.rectangle import Rectangle
+from game_engine.components.material import Material
 
 import math
 
@@ -20,19 +21,20 @@ class ObstaclesController(NormalBehavior):
             self.generate_simple_obstacle()
 
         for obstacle in self.game_object_list:
-            if (obstacle.rectangle_mash.position_y > Engine.screen_height):
+            if obstacle.transform.position.x > Engine.screen_height:
                 obstacle.destroy()
             else:
                 self.fall(obstacle)
 
     def fall(self, obstacle):
-        obstacle.transform.translate = (obstacle.transform.translate[0], obstacle.transform.translate[1]
+        obstacle.transform.position = Vector2(obstacle.transform.position.x, obstacle.transform.position.y
                                         + self.fall_velocity * Time.delta_time())
 
     def generate_simple_obstacle(self):
         self.last_generation_time = 1000 * Time.now()
 
         direction = rand(0,1) < 0.5
-        rect = Rectangle(direction * 0.6 * Engine.screen_width, - 0.1 * Engine.screen_height, 0.4 * Engine.screen_width,
-                         0.1 * Engine.screen_height, (255, 255, 255))
+        rect = Rectangle(Vector2(direction * 0.6 * Engine.screen_width, - 0.1 * Engine.screen_height),
+                         Vector2(0.4 * Engine.screen_width,0.1 * Engine.screen_height),
+                         Material((255, 255, 255)))
         self.game_object_list.append(rect)
