b7110b3256787b398ace806dedf6e6b20c1606c3
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-03-15 14:48:16 -0300

    Make the game asynchronous

--- game_engine/engine.py
+++ game_engine/engine.py
@@ -2,7 +2,7 @@ import pygame
 from .draw import Draw
 from game_engine.components.constants import Constants
 from game_engine.scene import Scene
-
+from game_engine.time import Time
 
 class Engine:
 
@@ -13,14 +13,24 @@ class Engine:
     @classmethod
     def start_game(cls, game_name, scenes):
         """
-        Start the game with pygame
+        Start the game coroutine with pygame
         :param game_name: name that will be displayed on the window
         :param scenes: list of scenes of the game
         """
+        cls.game_name = game_name
+        cls.scenes = scenes
+        Time.start_coroutine(cls.game)
+        Time.start_game()
+
+    @classmethod
+    async def game(cls):
+        """
+        Async method that will be the coroutine where the game will run in
+        """
         pygame.init()
         cls.game_display = pygame.display.set_mode((Constants.screen_width, Constants.screen_height))
-        pygame.display.set_caption(game_name)
-        Scene.scenes_list = scenes
+        pygame.display.set_caption(cls.game_name)
+        Scene.scenes_list = cls.scenes
         Draw.set_game_display(Engine.game_display)
         Scene.start_first_scene()
 
