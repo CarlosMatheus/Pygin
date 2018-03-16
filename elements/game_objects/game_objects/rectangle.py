1e6a4cdf5144f043c098243c46058d3e84d1c8cc
Eric Moreira <ericpqmor@gmail.com>
2018-03-16 18:48:43 -0300

    Now rectangle is a polygon

--- elements/game_objects/game_objects/rectangle.py
+++ elements/game_objects/game_objects/rectangle.py
@@ -1,5 +1,6 @@
 from elements.game_objects.game_objects.basic_objects.basic_rectangle import BasicRectangle
 from game_engine.components.box_collider import BoxCollider
+from game_engine.components.polygon_mesh import PolygonMesh
 from pygame.math import Vector2
 
 
@@ -7,4 +8,14 @@ class Rectangle(BasicRectangle):
 
     def __init__(self, position, dimension, material):
         super(Rectangle, self).__init__(position, dimension, material, layer = 0)
+        self.dimension = dimension
         self.box_collider = BoxCollider(self)
+        self.polygon_mesh = PolygonMesh(self, self.get_points(), material)
+
+    def get_points(self):
+        point_list = []
+        point_list.append(Vector2(self.transform.position.x, self.transform.position.y))
+        point_list.append(Vector2(self.transform.position.x, self.transform.position.y + self.dimension.y))
+        point_list.append(Vector2(self.transform.position.x + self.dimension.x, self.transform.position.y + self.dimension.y))
+        point_list.append(Vector2(self.transform.position.x + self.dimension.x, self.transform.position.y))
+        return point_list
