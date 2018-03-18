5294589ba02dd166dbcbfc44274e8396878eee72
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-03-18 19:24:22 -0300

    Adjust key_frame

--- game_engine/key_frames.py
+++ game_engine/key_frames.py
@@ -1,12 +1,20 @@
-from pygame.math import Vector2
-
-
 class KeyFrame:
 
-    def __init__(self, time, position=None, rotation=None, scale=None, layer=None, interpolation="in_cubic"):
+    def __init__(self, time, position=None, rotation=None, scale=None, layer=None, alpha=None, interpolation="in_cubic"):
+        """
+        Define one key_frame that will compound a list of key_frames that will be passed to an animation
+        :param time:
+        :param position:
+        :param rotation:
+        :param scale:
+        :param layer:
+        :param alpha:
+        :param interpolation:
+        """
         self.position = position
         self.rotation = rotation
         self.scale = scale
         self.layer = layer
+        self.alpha = alpha
         self.time = time
         self.interpolation = interpolation
