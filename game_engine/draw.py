3b69142889628dcdd15124f2bc951fc8b699e2db
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-03-18 19:17:29 -0300

    Add support to draw alpha color

--- game_engine/draw.py
+++ game_engine/draw.py
@@ -1,18 +1,23 @@
 import pygame
-
 from .color import Color
 
 
 class Draw:
     game_display = 0
+    screen_width = 0
+    screen_height = 0
 
     @classmethod
-    def set_game_display(cls, screen):
+    def set_game_display(cls, screen, screen_width, screen_height):
         """
         set the screen reference
         :param screen: the pygame's screen object
+        :param screen_width:
+        :param screen_height:
         """
         cls.game_display = screen
+        cls.screen_width = screen_width
+        cls.screen_height = screen_height
 
     @classmethod
     def update_background(cls):
@@ -22,35 +27,39 @@ class Draw:
         cls.game_display.fill(Color.black)
 
     @classmethod
-    def rect(cls, position, dimension, color):
+    def rect(cls, position, dimension, color, alpha=None):
         """
         Draw a rectangle
-        :param position_x: rect's x position
-        :param position_y: rect's y position
-        :param scale_x: rect's x scale
-        :param scale_y: rect's y scale
+        :param position: rect's position
+        :param dimension: rect's scale
         :param color: rect's color
+        :param alpha: rect's alpha value
+        :return:
         """
-        pygame.draw.rect(cls.game_display, color, [int(position.x), int(position.y), int(dimension.x), int(dimension.y)])
+        if alpha is not None:
+            s = pygame.Surface((int(dimension.x), int(dimension.y)))
+            s.set_alpha(alpha)
+            pygame.draw.rect(s, color, [int(position.x), int(position.y), int(dimension.x), int(dimension.y)])
+            cls.game_display.blit(s, (int(position.x), int(position.y)))
+        else:
+            pygame.draw.rect(cls.game_display, color, [int(position.x), int(position.y), int(dimension.x), int(dimension.y)])
 
     @classmethod
-    def circle(cls, position, radius, color):
+    def circle(cls, position, radius, color, alpha=None):
         """
         Draw a circle
-        :param position_x: circle's x position
-        :param position_y: circle's y position
+        :param position: circle's position
         :param radius: circle's radius
         :param color: circle's color
         """
         pygame.draw.circle(cls.game_display, color, [int(position.x), int(position.y)], int(radius))
 
     @classmethod
-    def polygon(cls, color, point_list):
+    def polygon(cls, color, point_list, alpha=None):
         """
         Draw a polygon
         :param color:
         :param point_list:
-        :return:
         """
         pygame.draw.polygon(cls.game_display, color, point_list)
 
@@ -67,6 +76,5 @@ class Draw:
         :return:
         """
         font = pygame.font.Font(font_path, size)
-        # render text
         label = font.render(message, 1, color)
         cls.game_display.blit(label, (position_x, position_y))
