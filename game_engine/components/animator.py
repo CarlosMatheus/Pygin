0985e909cf4860f749d1db3790262ea9ff779657
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-03-23 10:46:24 -0300

    Finish animator class

--- game_engine/components/animator.py
+++ game_engine/components/animator.py
@@ -1,4 +1,3 @@
-from game_engine.key_frames import KeyFrame
 from game_engine.component import Component
 
 
@@ -6,7 +5,8 @@ class Animator(Component):
 
     def __init__(self, game_object, animation_list):
         """
-        :param game_object:
+        Initiate Animator with the animation list
+        :param game_object: the list of animations for this animator
         """
         self.animation_list = animation_list
         self.current_playing_animation = None
@@ -15,20 +15,65 @@ class Animator(Component):
             animation.set_animator(self)
         super().__init__(game_object)
 
-    def play(self, animation_idx=0):
+    def play(self, animation_idx=0, should_loop=False, loops="inf"):
         """
         will play the list of animation in sequence
-        :return:
         """
+        self.should_loop = should_loop
+        self.loops = loops
+        self.current_loop = 0
         self.animation_idx = animation_idx
         self.current_playing_animation = self.animation_list[self.animation_idx]
 
+    def play_next_animation(self):
+        """
+        Play the next animation on animation list
+        It checks before if it can play and if it should loop
+        """
+        if self.animation_idx < len(self.animation_list)-1:
+            self.__next()
+        elif self.should_loop:
+            self.__loop()
+        else:
+            self.stop()
+
+    def play_first_animation(self):
+        """
+        Play the first animation of the animation list of this animator
+        """
+        self.animation_idx = 0
+        self.animation_list[self.animation_idx].play(self.animation_idx)
+
     def stop(self):
+        """
+        stop playing animation
+        """
         self.current_playing_animation = None
 
-    def update(self):
+    def __next(self):
+        """
+        play next animation
+        """
+        self.animation_idx += 1
+        self.animation_list[self.animation_idx].play(self.animation_idx)
+
+    def __loop(self):
+        """
+        loop in animation list
+        """
+        if self.loops is "inf":
+            self.play_first_animation()
+        else:
+            if self.loops > self.current_loop:
+                self.current_loop += 1
+                self.play_first_animation()
+            else:
+                self.stop()
+
+    def __update(self):
+        """
+        This will run every frame
+        Will update the current animation
+        """
         if self.current_playing_animation is not None:
             self.current_playing_animation.update()
-
-    def play_next_animation(self):
-        self.animation_list[self.animation_idx].play(self.animation_idx)
