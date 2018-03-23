7c2f12a2a462fa6055c9a7788ecae3885c893048
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-03-23 10:56:24 -0300

    Add alpha channel to material

--- game_engine/components/material.py
+++ game_engine/components/material.py
@@ -3,10 +3,11 @@ from game_engine.component import Component
 
 class Material(Component):
 
-    def __init__(self, game_object, color):
+    def __init__(self, game_object, color, alpha=None):
         """
         set initial parameters
         :param color: material color
         """
         super(Material, self).__init__(game_object)
         self.color = color
+        self.alpha = alpha
