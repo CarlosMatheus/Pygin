1ce7d37123bb430f5006b442dff7719916516d74
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-03-25 17:16:18 -0300

    Refactor to scaled radius

--- game_engine/components/circle_mesh.py
+++ game_engine/components/circle_mesh.py
@@ -5,5 +5,14 @@ class CircleMesh(Mesh):
 
     def __init__(self, game_object, radius, material):
         super(CircleMesh, self).__init__(game_object)
-        self.radius = radius
+        self.__radius = radius
         self.material = material
+
+    def get_radius(self):
+        return self.__radius * max(self.transform.scale.x, self.transform.scale.y)
+    
+    def get_unscaled_radius(self):
+        return self.__radius
+
+    def set_radius(self, radius):
+        self.__radius = radius
