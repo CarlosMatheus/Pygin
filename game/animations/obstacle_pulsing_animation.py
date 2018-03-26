e07fe22a7a28f7bbd1b25fef938fd5ca9010f724
Eric Moreira <ericpqmor@gmail.com>
2018-03-25 21:20:11 -0300

    New pulsing animation

--- game/animations/obstacle_pulsing_animation.py
+++ game/animations/obstacle_pulsing_animation.py
@@ -9,9 +9,9 @@ class ObstaclePulsingAnimation(Animation):
     def __init__(self, game_obj):
         key_frame_list = list()
         key_frame_list.append(
-            KeyFrame(0.0, scale=Vector2(1.0, 1.0), interpolation="out_cubic"))
+            KeyFrame(0.00, scale=Vector2(1.0, 1.0), interpolation="out_cubic"))
         key_frame_list.append(
-            KeyFrame(0.5, scale=Vector2(2.0, 2.0), interpolation="out_cubic"))
+            KeyFrame(0.25, scale=Vector2(1.1, 1.1), interpolation="out_cubic"))
         key_frame_list.append(
-            KeyFrame(1.0, scale=Vector2(1.0, 1.0)))
+            KeyFrame(0.50, scale=Vector2(1.0, 1.0)))
         super().__init__(game_obj, key_frame_list, should_loop=True)
