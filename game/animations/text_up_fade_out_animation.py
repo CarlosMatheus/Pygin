c62999ffcf81b24392a6132e525ff30dcf8fc18b
igor-ribeiiro <ribeiro_igor@hotmail.com>
2018-04-01 16:12:12 -0300

    Making text for invencible power_up better

--- game/animations/text_up_fade_out_animation.py
+++ game/animations/text_up_fade_out_animation.py
@@ -3,6 +3,7 @@ from game_engine.key_frame import KeyFrame
 from game.scripts.constants import Constants
 from pygame.math import Vector2
 
+
 class TextUpFadeOutAnimation(Animation):
 
     def __init__(self, game_obj):
@@ -10,5 +11,5 @@ class TextUpFadeOutAnimation(Animation):
         key_frame_list.append(
             KeyFrame(0.00, position=game_obj.transform.position, alpha = 255, interpolation="in_out_quint"))
         key_frame_list.append(
-            KeyFrame(0.70, position=Vector2(game_obj.transform.position.x, 0.95 * game_obj.transform.position.y), alpha=0))
+            KeyFrame(0.8, position=Vector2(game_obj.transform.position.x, 0.95 * game_obj.transform.position.y), alpha=0))
         super().__init__(game_obj, key_frame_list, should_loop=False)
