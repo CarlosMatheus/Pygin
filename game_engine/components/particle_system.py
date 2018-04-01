a74be4eb23fd64207be9b833095df082505fd27d
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-04-01 13:49:15 -0300

    Add linear prob function

--- game_engine/components/particle_system.py
+++ game_engine/components/particle_system.py
@@ -1,17 +1,16 @@
-from game_engine.component import Component
 from game_engine.components.physics import Physics
-from _ignore.game_test.animations.disappear import Disappear
-from game_engine.components.animator import Animator
-from pygame.math import Vector2
+from game_engine.component import Component
 from game_engine.time import Time
+from pygame.math import Vector2
 import random
 import math
 
 
 class ParticleSystem(Component):
 
-    def __init__(self, game_object, spawn_game_obj_class, quant=1, period=0.05, vel_min=80, vel_max=160):
+    def __init__(self, game_object, spawn_game_obj_class, quant=1, period=0.05, vel_min=80, vel_max=160, duration=1.0):
         super().__init__(game_object)
+        self.duration = duration
         self.vel_min = vel_min
         self.vel_max = vel_max
         self.quant = quant
@@ -29,7 +28,7 @@ class ParticleSystem(Component):
         self.fin_point_method = fin_point_method
         self.generation_mode = self.set_line_gen
 
-    def set_circ_gen(self, center_point, radius, mode="radial", ini_angle_met=None, fin_angle_met=None):
+    def set_circ_gen(self, center_point, radius, mode="radial", ini_angle_met=None, fin_angle_met=None, direct_met=None):
         if ini_angle_met is None:
             self.ini_angle_met = self.default_ini_ang_met
         else:
@@ -38,6 +37,7 @@ class ParticleSystem(Component):
             self.fin_angle_met = self.default_fin_ang_met
         else:
             self.fin_angle_met = fin_angle_met
+        self.direct_met = direct_met
         self.center_point = center_point
         self.radius = radius
         self.mode = mode
@@ -64,31 +64,29 @@ class ParticleSystem(Component):
                 for i in range(self.quant):
                     spawn_location = None
                     obj_velocity_vect = None
+                    multiplier = self.__linear_prob_func()
                     if self.generation_mode == self.set_line_gen:
-                        multiplier = self.__circular_prob_func()
                         spawn_location = (self.fin_point_method() - self.ini_point_method()) * multiplier + self.ini_point_method()
                         velocity = random.randint(int(self.vel_min*1000), int(self.vel_max*1000))/1000
                         obj_velocity_vect = (self.fin_point_method() - self.ini_point_method()).normalize().rotate(90)*velocity
                     elif self.generation_mode == self.set_circ_gen:
-                        multiplier = self.__circular_prob_func()
                         normal = Vector2(1, 0).rotate((self.fin_angle_met() - self.ini_angle_met()) * multiplier + self.ini_angle_met())
                         spawn_location = self.center_point + normal * self.radius
                         velocity = random.randint(int(self.vel_min * 1000), int(self.vel_max * 1000)) / 1000
                         if self.mode == "radial":
                             obj_velocity_vect = normal * velocity
                         elif self.mode == "directional":
-                            ()
+                            obj_velocity_vect = self.direct_met() * velocity
                         else:
                             raise Exception("Unknown mode {0}".format(str(self.mode)))
-
                     obj = self.spawn_game_obj_class(spawn_location)
                     obj.physics = Physics(obj, velocity=obj_velocity_vect)
+                    obj.set_creator_object(self.game_object)
                     self.obj_list.append(obj)
                     self.destroy_first()
 
-
     def destroy_first(self):
-        if Time.now() - self.destroy_timer > 1.0:
+        if Time.now() - self.destroy_timer > self.duration:
             self.obj_list[0].destroy_me()
             del self.obj_list[0]
 
@@ -105,6 +103,9 @@ class ParticleSystem(Component):
     def __gauss_prob_func(self):
         # line going from 0 to 1
         return max(min(random.gauss(0.5, 0.25), 1.0), 0.0)
+
+    def __linear_prob_func(self):
+        return random.randint(0, 1000)/1000.0
 
     def should_spawn(self):
         if (Time.now() - self.last_time) > self.period:
