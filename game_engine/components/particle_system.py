89f24d124ce6bf0e12a2ce21e0c5fead18f26144
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-04-09 15:49:30 -0300

    Create parabolic function, inherit and gravity

--- game_engine/components/particle_system.py
+++ game_engine/components/particle_system.py
@@ -8,11 +8,15 @@ import math
 
 class ParticleSystem(Component):
 
-    def __init__(self, game_object, spawn_game_obj_class, quant=1, period=0.05, vel_min=80, vel_max=160, duration=1.0):
+    def __init__(self, game_object, spawn_game_obj_class, quant=1, period=0.05, vel_min=80, vel_max=160,
+                 duration=1.0, gravity=0, inherit_vel=False, inherit_vel_mult=1, multiplier="lin"):
         super().__init__(game_object)
         self.duration = duration
+        self.gravity = gravity
         self.vel_min = vel_min
         self.vel_max = vel_max
+        self.inherit_vel = inherit_vel
+        self.inherit_vel_mult = inherit_vel_mult
         self.quant = quant
         self.turned_on = False
         self.period = period
@@ -22,6 +26,11 @@ class ParticleSystem(Component):
         self.fin_point_method = None
         self.generation_mode = None
         self.obj_list = list()
+        self.multiplier = None
+        self.define_multiplier(multiplier)
+        if self.inherit_vel:
+            if self.game_object.physics is None:
+                self.game_object.physics = Physics(self.game_object)
 
     def set_line_gen(self, ini_point_method, fin_point_method):
         self.ini_point_method = ini_point_method
@@ -57,14 +66,19 @@ class ParticleSystem(Component):
     def stop(self):
         self.turned_on = False
 
-    def __update(self):
+    def define_multiplier(self, multiplier):
+        if multiplier == "lin":
+            self.multiplier = self.__linear_prob_func
+        elif multiplier == "parab":
+            self.multiplier = self.__parabolic_prob_func
 
+    def __update(self):
         if self.turned_on:
             if self.should_spawn():
                 for i in range(self.quant):
                     spawn_location = None
                     obj_velocity_vect = None
-                    multiplier = self.__linear_prob_func()
+                    multiplier = self.multiplier()
                     if self.generation_mode == self.set_line_gen:
                         spawn_location = (self.fin_point_method() - self.ini_point_method()) * multiplier + self.ini_point_method()
                         velocity = random.randint(int(self.vel_min*1000), int(self.vel_max*1000))/1000
@@ -80,7 +94,11 @@ class ParticleSystem(Component):
                         else:
                             raise Exception("Unknown mode {0}".format(str(self.mode)))
                     obj = self.spawn_game_obj_class(spawn_location)
-                    obj.physics = Physics(obj, velocity=obj_velocity_vect)
+                    if self.inherit_vel:
+                        obj.physics = Physics(obj, velocity=obj_velocity_vect + self.game_object.physics.inst_velocity*self.inherit_vel_mult)
+                    else:
+                        obj.physics = Physics(obj, velocity=obj_velocity_vect)
+                    obj.physics.gravity = self.gravity
                     obj.set_creator_object(self.game_object)
                     self.obj_list.append(obj)
                     self.destroy_first()
@@ -99,6 +117,14 @@ class ParticleSystem(Component):
             return (1 - math.cos(rad)) / 2
         else:
             return (1 - math.cos(rad)) / 2
+
+    def __parabolic_prob_func(self):
+        x = random.randint(0, 100)
+        odd = random.randint(0, 100)
+        if odd < 50:
+            return ((-1)*math.sqrt(x/100)+1)/2
+        else:
+            return (math.sqrt(x/100)+1)/2
 
     def __gauss_prob_func(self):
         # line going from 0 to 1
