931a780b7b1dabd1084e1b441e1271cb8313b6b9
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-04-08 23:35:36 -0300

    Add fade animation to particle

--- game/game_objects/mesh_objects/player_particle.py
+++ game/game_objects/mesh_objects/player_particle.py
@@ -1,15 +1,21 @@
 from game_engine.basic_objects.basic_particle_circ import BasicParticleCirc
+from game.animations.particle_fade_animation import ParticleFadeAnimation
+from game_engine.material import Material
+from game_engine.components.animator import Animator
 
 
 class PlayerParticle(BasicParticleCirc):
 
     def __init__(self, position):
+        self.change = True
         super().__init__(position)
 
     def start(self):
-        self.change = True
+        self.animation = ParticleFadeAnimation(self, self.creator_obj.particle_system.duration)
+        self.animator = Animator(self, [self.animation])
+        self.animator.play()
 
     def update(self):
         if self.change:
             self.change = False
-            self.material = self.creator_obj.material
+            self.material = Material(self.creator_obj.material.color)
