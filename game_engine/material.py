f93799228fb1b93999259aee5ae588431fff3959
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-04-08 22:53:07 -0300

    Refactor material alpha

--- game_engine/material.py
+++ game_engine/material.py
@@ -3,7 +3,7 @@ from game_engine.color import Color
 
 class Material:
 
-    def __init__(self, color=Color.white, alpha=None):
+    def __init__(self, color=Color.white, alpha=255):
         """
         set initial parameters
         :param color: material color
