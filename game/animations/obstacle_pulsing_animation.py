5b12764f260b4805e7bf10e16d043e2c3f99f4b1
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-04-09 15:56:27 -0300

    Refactor pulsing and combine with translate

--- game/animations/obstacle_pulsing_animation.py
+++ game/animations/obstacle_pulsing_animation.py
@@ -2,16 +2,26 @@ from game_engine.components.animation import Animation
 from game_engine.key_frame import KeyFrame
 from game.scripts.constants import Constants
 from pygame.math import Vector2
+import random
 
 
 class ObstaclePulsingAnimation(Animation):
 
     def __init__(self, game_obj):
+        gap = 0.7
         key_frame_list = list()
-        key_frame_list.append(
-            KeyFrame(0.00, scale=Vector2(1.0, 1.0), interpolation="out_cubic"))
-        key_frame_list.append(
-            KeyFrame(0.25, scale=Vector2(1.1, 1.1), interpolation="out_cubic"))
-        key_frame_list.append(
-            KeyFrame(0.50, scale=Vector2(1.0, 1.0)))
+        key_frame_list.append(KeyFrame(0.00,
+                                       scale=Vector2(1.0, 1.0),
+                                       position=Vector2(0, 0),
+                                       interpolation="out_cubic"))
+        key_frame_list.append(KeyFrame(0.35,
+                                       scale=Vector2(1.07, 1.07),
+                                       position=Vector2(self.rand()*gap, self.rand()*gap),
+                                       interpolation="out_cubic"))
+        key_frame_list.append(KeyFrame(0.70,
+                                       scale=Vector2(1.0, 1.0),
+                                       position=Vector2(0, 0)))
         super().__init__(game_obj, key_frame_list, should_loop=True)
+
+    def rand(self):
+        return random.randint(-2, 8)
