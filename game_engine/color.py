a91591741b1ca689fe6f0510ee7b741b53127cad
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-04-10 13:05:12 -0300

    Add some colors

--- game_engine/color.py
+++ game_engine/color.py
@@ -6,10 +6,12 @@ class Color:
     red = (255, 0, 0)
     yellow = (247, 251, 0)
     blue = (36, 127, 244)
+    blue_0 = (102, 102, 255)
     green = (86, 244, 85)
     silver = (192, 192, 192)
     gray = (112, 112, 112)
     orange = (253, 102, 0)
+    purple = (244, 113, 244)
 
     @classmethod
     def random_color(cls):
