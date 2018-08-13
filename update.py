30e6c894193e12fd7c0d385d7634be17002eb992
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2018-08-12 23:40:28 -0300

    Try to make update

---
+++ update.py
@@ -0,0 +1,17 @@
+import subprocess
+import os
+path = os.path.realpath("pygin/_version.py")
+root_path = os.path.realpath("")
+
+version_file = open(path, 'w')
+version = input("What will be this version? ")
+version_file.write("def get_version():\n    return '" + str(version) + "'\n")
+
+bashCommands = ["cd " + root_path + " ; python -m pip install --upgrade setuptools wheel",
+                "cd " + root_path + " ; python setup.py sdist",
+                "cd " + root_path + " ; python -m pip install --upgrade twine",
+                "cd " + root_path + " ; twine upload --repository-url https://upload.pypi.org/legacy/ dist/*"]
+
+for bashCommand in bashCommands:
+    print(bashCommand)
+    process = subprocess.call(bashCommand.split())
